c8d9663b25c266c9c3c69f07ffa38fcac143188b
Nicholas Coughlin <coughlin.nicholas@gmail.com>
2020-09-13 14:51:09 -0700

    retreiving sign in status

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -2,19 +2,38 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 class GoogleAuth extends Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load("auth2", () => {
-      window.gapi.auth2.init({
-        client_id: process.env.REACT_APP_GOOGLE_OAUTH2_CLIENT_ID,
-        scope: "email",
-      });
+      window.gapi.auth2
+        .init({
+          client_id: process.env.REACT_APP_GOOGLE_OAUTH2_CLIENT_ID,
+          scope: "email",
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          // update state so that component will re-render
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
     });
+  }
+
+  // helper function
+  retrieveUserStatus() {
+    if (this.state.isSignedIn === null) {
+      return "UNKNOWN";
+    } else if (this.state.isSignedIn) {
+      return "SIGNED IN";
+    } else {
+      return "NOT SIGNED IN";
+    }
   }
 
   render() {
     return (
       <Link to="/" className="item">
-        Google Auth
+        <div>Status: {this.retrieveUserStatus()}</div>
       </Link>
     );
   }
