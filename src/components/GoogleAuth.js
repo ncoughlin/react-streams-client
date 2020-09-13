4446a114f076ba206592584d5fd1e6e794b2a2e7
Nicholas Coughlin <coughlin.nicholas@gmail.com>
2020-09-13 12:52:03 -0700

    now using environment variables

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,22 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 class GoogleAuth extends Component {
-    componentDidMount() {
-        window.gapi.load('client:auth2', () => {
-            window.gapi.client.init({ 
-                clientId: process.env.GOOGLE_OAUTH2_CLIENT_ID
-             });
-        });
-    }
+  componentDidMount() {
+    window.gapi.load("auth2", () => {
+      window.gapi.auth2.init({
+        client_id: process.env.REACT_APP_GOOGLE_OAUTH2_CLIENT_ID,
+        scope: "email",
+      });
+    });
+  }
 
-    render() {
-        return (
-            <Link to="/" className="item">
-               Google Auth
-            </Link>
-        );
-    }
+  render() {
+    return (
+      <Link to="/" className="item">
+        Google Auth
+      </Link>
+    );
+  }
 }
 
 export default GoogleAuth;
