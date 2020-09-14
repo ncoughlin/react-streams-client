d6415031a7831088618aba5ac06d7c5838f80646
Nicholas Coughlin <coughlin.nicholas@gmail.com>
2020-09-14 12:08:52 -0700

    scaffolding redux

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -28,11 +28,11 @@ class GoogleAuth extends Component {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   };
 
-  onSignIn = () => {
+  onSignInClick = () => {
     this.auth.signIn();
   }
 
-  onSignOut = () => {
+  onSignOutClick = () => {
     this.auth.signOut();
   }
 
@@ -42,14 +42,14 @@ class GoogleAuth extends Component {
       return null;
     } else if (this.state.isSignedIn) {
       return (
-        <button onClick={this.onSignOut} className="ui red google button">
+        <button onClick={this.onSignOutClick} className="ui red google button">
           <i className="google icon" />
           Sign Out
         </button>
       );
     } else {
       return (
-        <button onClick={this.onSignIn} className="ui red google button">
+        <button onClick={this.onSignInClick} className="ui red google button">
           <i className="google icon" />
           Sign In
         </button>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from "react";
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-import App from './components/App';
+import App from "./components/App";
+import reducers from "./reducers";
+
+const store = createStore(reducers);
 
 ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
 );
