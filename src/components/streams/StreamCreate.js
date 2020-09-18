2967d3db8adce188a6cc6f5ee656a06e72e25e24
Nicholas Coughlin <coughlin.nicholas@gmail.com>
2020-09-18 15:15:07 -0700

    submission button added

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 class StreamCreate extends Component {
   renderInput({ input, label }) {
-    console.log(input)
     return (
       <div className="field">
         <label>{label}</label>
@@ -11,11 +10,16 @@ class StreamCreate extends Component {
     );
   }
 
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
   render() {
     return (
-      <form className="ui form">
+      <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
         <Field name="Title" component={this.renderInput} label="Enter Title: " />
         <Field name="Description" component={this.renderInput} label="Enter Description: " />
+        <button className="ui button primary">Submit</button>
       </form>
     );
   }
