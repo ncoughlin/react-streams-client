e9ef1591c5f8f2c7dfab3b3c44657c6918eb40a5
Nicholas Coughlin <coughlin.nicholas@gmail.com>
2020-09-28 12:23:48 -0700

    creating REST redux actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,13 @@
 import streams from '../apis/streams'
-import { CREATE_STREAM, SIGN_IN, SIGN_OUT } from './types'
+import {  
+  SIGN_IN, 
+  SIGN_OUT,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  CREATE_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM
+} from './types'
 
 // change user status to signed in
 export const signIn = (userId) => {
@@ -19,7 +27,21 @@ export const signOut = () => {
 
 // create stream
 export const createStream = formValues => async (dispatch) => {
-  const response = await streams.post('./streams', formValues);
+  const response = await streams.post('/streams', formValues);
 
   dispatch({ type: CREATE_STREAM, payload:response.data });
 };
+
+// fetch streams
+export const fetchStreams = () => async dispatch => {
+  const response = await streams.get('/streams');
+
+  dispatch({ type: FETCH_STREAMS, payload: response.data })
+};
+
+// fetch stream
+export const fetchStream = (streamId) => async dispatch => {
+  const response = await streams.get(`/streams/${streamId}`)
+
+  dispatch({ type: FETCH_STREAM, payload: response.data })
+}
