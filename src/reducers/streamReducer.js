f1e5c46fc4051855a4e898390675f4cd40631cb4
Nicholas Coughlin <coughlin.nicholas@gmail.com>
2020-10-07 17:06:37 -0300

    scaffolding reducers

---
+++ src/reducers/streamReducer.js
@@ -0,0 +1,18 @@
+import { FETCH_STREAMS, FETCH_STREAM, CREATE_STREAM, EDIT_STREAM, DELETE_STREAM } from '../actions/types'
+
+const streamReducer = (state={}, action) => {
+    switch (action.type) {
+        case FETCH_STREAMS:
+
+        case FETCH_STREAM:
+
+        case CREATE_STREAM:
+
+        case EDIT_STREAM:
+
+        case DELETE_STREAM:
+
+        default:
+            return state;
+    }
+};
