ca928df9a72176d4e10debda3e7b6ea055f3ccac
Nicholas Coughlin <coughlin.nicholas@gmail.com>
2020-10-13 18:06:48 -0300

    stream data loading independently and showing on StreamEdit component

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-const StreamEdit = () => {
+import { fetchStream } from "../../actions";
+
+class StreamEdit extends Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    // if we have not retrieved stream yet use loading animation
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
     return (
-        <div>
-            StreamEdit
-        </div>
+      <> 
+        <h1>{this.props.stream.title}</h1>
+        <p>{this.props.stream.description}</p>
+      </>
     );
+  }
 }
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+  // the stream that matches the ID in the URL
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
