468fc78fffd40f20204b9b8f9c2f66c56c396e52
Nicholas Coughlin <coughlin.nicholas@gmail.com>
2020-10-19 15:59:47 -0300

    redirect still not working...

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 import { Redirect } from "react-router-dom";
 
 const Modal = () => {
+
+  
   return ReactDOM.createPortal(
     <div
       className="ui dimmer modals visible active"
-      onClick={() => {
+      onClick={ () => {
         return <Redirect to="/" />;
-      }}
+      } }
     >
       <div className="ui standard modal visible active">
         <div className="header">Delete Stream</div>
