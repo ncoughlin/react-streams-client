cf02a1d5d968bd12a8c442f0ebb9588a1a6adace
Nicholas Coughlin <coughlin.nicholas@gmail.com>
2020-10-19 16:40:45 -0300

    modal now closing properly

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,29 +2,44 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Redirect } from "react-router-dom";
 
-const Modal = () => {
+class Modal extends React.Component {
+  state = {
+    toDashboard: false,
+  }
 
+  handleClick = () => {
+    this.setState(() => ({
+      toDashboard: true
+    }))
+  }
+
+
+  render() {
+    if (this.state.toDashboard === true) {
+      return <Redirect to='/' />
+    }
+
+    return ReactDOM.createPortal(
+      
+      <div
+        className="ui dimmer modals visible active"
+        onClick={ this.handleClick }
+      >
+        <div className="ui standard modal visible active">
+          <div className="header">Delete Stream</div>
+          <div className="content">
+            Are you sure you want to delete this stream?
+          </div>
+          <div className="actions">
+            <button className="ui primary button">Delete</button>
+            <button className="ui button">Cancel</button>
+          </div>
+        </div>
+      </div>,
+      document.querySelector("#modal")
+    );
+  }
   
-  return ReactDOM.createPortal(
-    <div
-      className="ui dimmer modals visible active"
-      onClick={ () => {
-        return <Redirect to="/" />;
-      } }
-    >
-      <div className="ui standard modal visible active">
-        <div className="header">Delete Stream</div>
-        <div className="content">
-          Are you sure you want to delete this stream?
-        </div>
-        <div className="actions">
-          <button className="ui primary button">Delete</button>
-          <button className="ui button">Cancel</button>
-        </div>
-      </div>
-    </div>,
-    document.querySelector("#modal")
-  );
 };
 
 export default Modal;
